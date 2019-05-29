8b8c3239c280a51eb25c2acb7dd6e9771d97b0d0
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-05-29 15:37:05 +0200

    rewrote localStorage handling

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { render } from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import appReducer from './reducers/'
+import { loadState, saveState } from './modules/localStorage'
 
 // Components
 import Router from './components/Router'
@@ -17,11 +18,17 @@ import "./sass/style.scss";
 // Create Redux store and initialize the app
 const store = createStore(
   appReducer,
+  loadState(),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
 
 render(
   <Provider store={store}>
     <Router />
   </Provider>,
-  document.querySelector('#main'));
+  document.querySelector('#main')
+);
