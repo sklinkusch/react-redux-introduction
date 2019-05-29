a1c3536315956f07ef0a5ad8445927008cb7894e
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-05-29 11:10:38 +0200

    replaced React.Fragment by header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,14 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 
 export default class Header extends Component {
   render() {
     return (
-      <Fragment>
+      <header className="header">
         <h1>
           What to do?
       </h1>
         <span className="tagline">This could be your bucket list.</span>
-      </Fragment>
+      </header>
     )
   }
 }
