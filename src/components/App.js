7ef9e584fc1cdeda5dcdbf374e12614f726f7c9a
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-05-29 11:47:04 +0200

    used ToDoForm in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 import Header from './Header';
 import ToDoList from './ToDoList';
 import { getRandomTagline } from '../helpers';
+import ToDoForm from './ToDoForm';
 
 export default class App extends Component {
   render() {
     return (
       <div className="container">
         <Header tagline={getRandomTagline()} />
+        <ToDoForm />
         <ToDoList />
       </div>
     )
