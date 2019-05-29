ba74b5f7597100ac92e915163fa281aef81398d0
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-05-29 10:43:55 +0200

    inserted Header

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-render(<p>Hello World!</p>, document.querySelector('#main'));
+class Header extends React.Component {
+  render() {
+    return (
+      <h1>
+        What to do?
+      </h1>
+    )
+  }
+}
+
+
+render(<Header />, document.querySelector('#main'));
