dbdf899a33cdc42462719a2f1e405aab684e6076
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-05-29 13:21:13 +0200

    added props in ToDoList, passed down to ToDoItem

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -5,11 +5,19 @@ export default class ToDoList extends Component {
   render() {
     return (
       <div className="todo-list">
-        <ul className="todo-items">
-          {Object.keys(this.props.items).map(uuid => (
-            <ToDoItem key={`todo-item-${uuid}`} data={this.props.items[uuid]} />
-          ))}
-        </ul>
+        <table className="todo-items table table-borderless">
+          <tbody>
+            {Object.keys(this.props.items).map(uuid => (
+              <ToDoItem
+                key={`todo-item-${uuid}`}
+                data={this.props.items[uuid]}
+                updateToDoText={this.props.updateToDoText}
+                toggleToDoDone={this.props.toggleToDoDone}
+                removeToDo={this.props.removeToDo}
+              />
+            ))}
+          </tbody>
+        </table>
       </div>
     )
   }
