c545853ea5c929d4f49ec91b2829d4de1fc6c8c7
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-05-29 11:08:52 +0200

    imported App in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
-import Header from './components/Header'
+import App from './components/App'
 
 import "./sass/style.scss";
 
 
-render(<Header />, document.querySelector('#main'));
+render(<App />, document.querySelector('#main'));
