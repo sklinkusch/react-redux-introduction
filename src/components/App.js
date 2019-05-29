3fa6bee8ff1aefbf6d42caedc9604ff9622b7c87
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-05-29 11:36:05 +0200

    used function to generate random tagline in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import Header from './Header';
 import ToDoList from './ToDoList';
+import { getRandomTagline } from '../helpers';
 
 export default class App extends Component {
   render() {
     return (
       <div className="container">
-        <Header tagline="This could be your shopping list." />
+        <Header tagline={getRandomTagline()} />
         <ToDoList />
       </div>
     )
