de546982fd7d9202d5b6fb28dc8fe7b20b88c12b
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-05-29 11:03:02 +0200

    imported stylesheet

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import Header from './components/Header'
 
+import "./sass/style.scss";
+
 
 render(<Header />, document.querySelector('#main'));
