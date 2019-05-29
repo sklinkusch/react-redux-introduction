43c6556617348f2f0c9c11423ec0d089604bd847
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-05-29 11:49:23 +0200

    added and used handleSubmit method

--- src/components/ToDoForm.js
+++ src/components/ToDoForm.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
 
 export default class ToDoForm extends Component {
+  handleSubmit() {
+    e.preventDefault();
+    console.log("Create new item");
+  }
   render() {
     return (
-      <form className="input-group my-3">
+      <form className="input-group my-3" onSubmit={this.handleSubmit}>
         <input className="form-control" type="text" placeholder="Add a new to-do item ..." />
         <div className="input-group-append">
           <button className="btn btn-outline-secondary" type="submit">
