a19c17dee68fe71c07e86e178c5fbef8b9d5f8a5
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-05-29 14:49:23 +0200

    created store in index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
+// React
 import React from 'react'
 import { render } from 'react-dom'
+
+// Redux
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import appReducer from './reducers/'
+
+// Components
 import Router from './components/Router'
 
+// Styles
 import "@fortawesome/fontawesome-free/css/all.css";
 import "./sass/style.scss";
 
+// Create Redux store and initialize the app
+const store = createStore(
+  appReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
-render(<Router />, document.querySelector('#main'));
+render(
+  <Provider store={store}>
+    <Router />
+  </Provider>,
+  document.querySelector('#main'));
